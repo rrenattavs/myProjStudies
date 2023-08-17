796e9e688cf2684256ca6d6ba24fb9a04d9e4a44
Renata Vieira <rrenattavs@gmail.com>
2023-08-16 22:23:27 -0600

    fixed card

--- MemoryGame/magic-game/src/components/SingleCard.jsx
+++ MemoryGame/magic-game/src/components/SingleCard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./SingleCard.css";
-const SingleCard = () => {
+const SingleCard = ({ card }) => {
   return (
     <div className="card">
-      <img className="front" src={card.src} alt="card front" />
-      <img className="back" src="/img/cover.png" alt="card back" />
+      <div>
+        <img className="front" src={card.src} alt="card front" />
+        <img className="back" src="/img/cover.png" alt="card back" />
+      </div>
     </div>
   );
 };
