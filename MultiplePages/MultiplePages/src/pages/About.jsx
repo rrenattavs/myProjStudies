5570c9251dcb51e3aa03cd4793bb59793ed20f9d
Renata Vieira <rrenattavs@gmail.com>
2023-09-05 17:56:29 -0600

    added another paragraph

--- MultiplePages/MultiplePages/src/pages/About.jsx
+++ MultiplePages/MultiplePages/src/pages/About.jsx
@@ -8,6 +8,12 @@ const About = () => {
         error ducimus illo enim sit fuga suscipit labore sint, sed, placeat
         animi iure!
       </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+        facilis voluptate omnis quisquam dolores veritatis esse placeat
+        molestias quia adipisci quas amet nihil, ratione perferendis a molestiae
+        temporibus voluptatem ab.
+      </p>
     </div>
   );
 };
